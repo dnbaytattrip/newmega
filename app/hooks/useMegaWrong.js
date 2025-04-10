902221f10d57f5865d80a7a8acdf897c58b549b7
rana <rana.buddy@gmail.com>
2025-04-10 18:02:31 +0600

    feat:first commit

--- app/hooks/useMegaWrong.js
+++ app/hooks/useMegaWrong.js
@@ -1,39 +1,39 @@
-"use client";
-import { toast } from "react-toastify";
-import Cookies from "js-cookie";
-import { API_URL } from "../config/index";
-import { useRouter } from "next/navigation";
-function useMegaWrong() {
-  console.log(adminId, posterId);
-  const router = useRouter();
-  const login = async (values) => {
-    // console.log(values);
+// "use client";
+// import { toast } from "react-toastify";
+// import Cookies from "js-cookie";
+// import { API_URL } from "../config/index";
+// import { useRouter } from "next/navigation";
+// function useMegaWrong() {
+//   console.log(adminId, posterId);
+//   const router = useRouter();
+//   const login = async (values) => {
+//     // console.log(values);
 
-    const url = `${API_URL}/mega/wrong`;
+//     const url = `${API_URL}/mega/wrong`;
 
-    const res = await fetch(url, {
-      method: "POST",
-      headers: {
-        Accept: "application/json",
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(values),
-    });
-    const data = await res.json();
-    console.log(data);
+//     const res = await fetch(url, {
+//       method: "POST",
+//       headers: {
+//         Accept: "application/json",
+//         "Content-Type": "application/json",
+//       },
+//       body: JSON.stringify(values),
+//     });
+//     const data = await res.json();
+//     console.log(data);
 
-    if (res.ok) {
-      console.log("success", data);
-    //   Cookies.set("email", data?.info?.email);
-    //   Cookies.set("id", data?.info?._id);
-      router.push("/map");
-    } else {
-      console.log("error", data);
-      toast.error("Something Went Wrong");
-    }
-  };
+//     if (res.ok) {
+//       console.log("success", data);
+//     //   Cookies.set("email", data?.info?.email);
+//     //   Cookies.set("id", data?.info?._id);
+//       router.push("/map");
+//     } else {
+//       console.log("error", data);
+//       toast.error("Something Went Wrong");
+//     }
+//   };
 
-  return { login };
-}
+//   return { login };
+// }
 
-export default useMegaWrong;
+// export default useMegaWrong;
